301c906a6583a9bd7028e7d26581737dbce2ae9a
José Fernandes de Freitas Junior <jffreitasjr@yahoo.com.br>
2020-11-18 18:44:40 -0300

    Add de imagem, formatação de estilo e correção erro no código para direcionar a cobrinha

--- Snake Game/script.js
+++ Snake Game/script.js
@@ -33,11 +33,11 @@ function drawFood(){
 
 document.addEventListener('keydown', update);
 
-function update(event){
-    if(event.keycode == 37 && direction != "right") direction = "left";
-    if(event.keycode == 38 && direction != "down") direction = "up";
-    if(event.keycode == 39 && direction != "left") direction = "right";
-    if(event.keycode == 40 && direction != "up") direction = "down";
+function update (event){
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
 }
 
 function iniciarJogo(){
@@ -46,6 +46,13 @@ function iniciarJogo(){
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game Over :(');
+        }
+    }
 
     criarBG();
     criarCobrinha();
@@ -65,9 +72,6 @@ function iniciarJogo(){
         food.x = Math.floor(Math.random()* 15 + 1) * box;
         food.y = Math.floor(Math.random()* 15 + 1) * box;
     }
-
-    
-
 
     let newHead = {
         
